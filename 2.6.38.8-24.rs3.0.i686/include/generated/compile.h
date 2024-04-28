ab0f9b4ae20b4ed81f7c90551e2b294ce7f4efc0
Robert Menes <rmenes379@protonmail.com>
2024-04-28 18:05:25 -0400

    Removing directory; recommiting with more logical layout

--- 2.6.38.8-24.rs3.0.i686/include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP */
-#define UTS_MACHINE "i386"
-#define UTS_VERSION "#1 SMP Fri Mar 22 09:35:36 KST 2013"
-#define LINUX_COMPILE_TIME "09:35:36"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "RS3-Build-Host"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.0 20090506 (Red Hat 4.4.0-4) (GCC) "
--- 2.6.38.8-24.rs3.0.i686/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "2.6.38.8-24.rs3.0.i686"
